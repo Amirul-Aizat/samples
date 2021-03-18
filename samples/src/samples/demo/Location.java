30ec930498dc44c2046220233c07eda7c29a70cf
Amirul Aizat <b031910118@student.utem.edu.my>
2021-03-18 14:43:01 +0800

    Add more attribute

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -3,10 +3,11 @@ package samples.demo;
 public class Location {
 	
 	private int space;
+	private int storage;
 	
-	
-	public Location(int space) {
+	public Location(int space, int storage) {
 		this.space=space;
+		this.storage=storage;
 	}
 
 
@@ -17,6 +18,16 @@ public class Location {
 
 	public void setSpace(int space) {
 		this.space = space;
+	}
+
+
+	public int getStorage() {
+		return storage;
+	}
+
+
+	public void setStorage(int storage) {
+		this.storage = storage;
 	}
 	
 	
