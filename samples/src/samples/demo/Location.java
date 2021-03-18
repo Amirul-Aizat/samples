b83d7b6bf4a4a323b6c79ace68bfc6e209ad71af
Amirul Aizat <b031910118@student.utem.edu.my>
2021-03-18 14:41:18 +0800

    Modify Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,23 @@
+package samples.demo;
+
+public class Location {
+	
+	private int space;
+	
+	
+	public Location(int space) {
+		this.space=space;
+	}
+
+
+	public int getSpace() {
+		return space;
+	}
+
+
+	public void setSpace(int space) {
+		this.space = space;
+	}
+	
+	
+}
